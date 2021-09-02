0803e3ce3bb55e0a957a917a611c5d1e2aa804b6
Brandon <brandonbland90@gmail.com>
2021-09-02 01:20:07 -0400

    Added logo, finished top nav, footer.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const sideNav = document.getElementById("sideNav")
 const navCloseButton = document.getElementById("navCloseButton")
 const navOpenButton = document.getElementById("navOpenButton")
 const navLinks = document.querySelectorAll(".nav-link")
+const date = document.getElementById('date')
 
 // Assign event listeners for side menu open/close
 navCloseButton.addEventListener("click", () => {
@@ -18,3 +19,9 @@ navLinks.forEach((link) => {
     sideNav.classList.remove("nav-open")
   })
 })
+
+// Date
+const now = new Date()
+const year = now.getFullYear()
+
+date.innerHTML = year
