415043229cb4260acab0164a57e1667a43dbdefc
Brandon <brandonbland90@gmail.com>
2021-09-09 01:50:50 -0400

    Added Image attribution toggle functionality.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ const navOpenButton = document.getElementById("navOpenButton")
 const navLinks = document.querySelectorAll(".nav-link")
 const date = document.getElementById('date')
 
+const imageAttributionToggle = document.getElementById('imageAttributionToggle')
+const attributionListSymbol = document.getElementById('attributionListSymbol')
+const techStackImageAttributionList = document.getElementById('techStackImageAttributionList')
+
 // Assign event listeners for side menu open/close
 navCloseButton.addEventListener("click", () => {
   sideNav.classList.toggle("nav-open")
@@ -25,3 +29,18 @@ const now = new Date()
 const year = now.getFullYear()
 
 date.innerHTML = year
+
+// Add event listener for image attribution toggle
+imageAttributionToggle.addEventListener('click', () => {
+  // if open class is present, remove it, change symbol
+  if (techStackImageAttributionList.classList.contains('open')) {
+    techStackImageAttributionList.classList.remove('open')
+    attributionListSymbol.innerHTML = "+"
+  } else {
+    // Add open class
+    techStackImageAttributionList.classList.add('open')
+    attributionListSymbol.innerHTML = '-'
+  }
+
+
+})
